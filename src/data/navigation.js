5b1fbbb1a8cf65d07bc050685ec4add99c96cb6d
callmeAngga <wongfromindo@gmail.com>
2025-06-01 08:29:32 +0700

    initial project

---
+++ src/data/navigation.js
@@ -0,0 +1,51 @@
+const menuNavigation = [
+    {
+        title: 'Dashboard',
+        icon: '',
+        path: '/'
+    },
+    {
+        title: 'Pencarian',
+        icon: '',
+        subMenu: [
+            {
+                title: 'Konverter Jawa',
+                icon: '',
+                path: '/konverter-jawa'
+            },
+            {
+                title: 'Konverter Indonesia',
+                icon: '',
+                path: '/konverter-indonesia'
+            },
+            {
+                title: 'Konverter Aksara Jawa',
+                icon: '',
+                path: '/konverter-aksara-jawa'
+            },
+            {
+                title: 'SPARQL Console',
+                icon: '',
+                path: '/sparql-console'
+            }
+        ]
+    },
+    {
+        title: 'Data Eksternal',
+        icon: '',
+        subMenu: [
+            {
+                title: 'Daftar Sumber Data',
+                icon: '',
+                path: '/daftar-sumber-data'
+            }
+        ]
+    },
+        {
+        title: 'Manajemen Data',
+        icon: '',
+        path: '/manajemen-data'
+    }
+]
+
+export default menuNavigation;
